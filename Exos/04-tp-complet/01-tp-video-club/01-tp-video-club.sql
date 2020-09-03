b39a1372fb1276f090e31ccc75c5159d13934589
kyweez <auboudier.pro@gmail.com>
2020-09-03 14:50:52 +0200

    J'en ai marre

--- Exos/04-tp-complet/01-tp-video-club/01-tp-video-club.sql
+++ Exos/04-tp-complet/01-tp-video-club/01-tp-video-club.sql
@@ -4,65 +4,94 @@ USE video_club;
 
 CREATE TABLE `Types`
 (
-	type_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    type_name VARCHAR(50) NOT NULL,
-    type_public VARCHAR(50) NOT NULL
-)ENGINE = InnoDB CHARSET utf8 COLLATE utf8_general_ci;
+	`type_id` INT(10) NOT NULL AUTO_INCREMENT,
+    `type_name` VARCHAR(50) NOT NULL,
+    `type_public` VARCHAR(50) NOT NULL,
+	
+    CONSTRAINT PK_Type PRIMARY KEY (`type_id`)
+)
+ENGINE = InnoDB 
+CHARSET utf8 
+COLLATE utf8_general_ci;
 
-CREATE TABLE Movies
+CREATE TABLE `Movies`
 (
-	movie_title VARCHAR(50) NOT NULL,
-    movie_director VARCHAR(100) NOT NULL,
-    PRIMARY KEY(movie_title, movie_director),
-    movie_duration SMALLINT(3) NOT NULL
-)ENGINE = InnoDB CHARSET utf8 COLLATE utf8_general_ci;
+	`movie_title` VARCHAR(50) NOT NULL,
+    `movie_director` VARCHAR(100) NOT NULL,
+    CONSTRAINT PK_movies PRIMARY KEY(`movie_title`, `movie_director`),
+    `movie_duration` SMALLINT(3) NOT NULL
+)
+ENGINE = InnoDB 
+CHARSET utf8 
+COLLATE utf8_general_ci;
 
-CREATE TABLE Actors
+CREATE TABLE `Actors`
 (
-	actor_name VARCHAR(50) NOT NULL,
-	actor_firstname VARCHAR(50) NOT NULL,
-    actor_birth DATE,
-	actor_sex VARCHAR(20),
-    PRIMARY KEY(actor_name, actor_firstname, actor_birth)
-)ENGINE = InnoDB CHARSET utf8 COLLATE utf8_general_ci;
+	`actor_name` VARCHAR(50) NOT NULL,
+	`actor_firstname` VARCHAR(50) NOT NULL,
+    `actor_birth` DATE,
+	`actor_sex` VARCHAR(20),
+    CONSTRAINT PK_actors PRIMARY KEY(`actor_name`, `actor_firstname`, `actor_birth`)
+)
+ENGINE = InnoDB
+CHARSET utf8
+COLLATE utf8_general_ci;
 
-CREATE TABLE Tape
+CREATE TABLE `Tapes`
 (
-	tape_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tape_registration DATETIME NOT NULL,
-    tape_state VARCHAR(50) NOT NULL
-)ENGINE = InnoDB CHARSET utf8 COLLATE utf8_general_ci;
+	`tape_id` INT(10) NOT NULL AUTO_INCREMENT,
+	`tape_registration` DATETIME NOT NULL,
+    `tape_state` VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_tapes PRIMARY KEY (`tape_id`)
+)
+ENGINE = InnoDB
+CHARSET utf8
+COLLATE utf8_general_ci;
 
-CREATE TABLE Addresses
+CREATE TABLE `Addresses`
 (
-	address_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    address_zipcode CHAR(5) NOT NULL, 
-    address_city VARCHAR(50) NOT NULL,
-    address_number VARCHAR(10),
-    address_street_name VARCHAR(100) NOT NULL
-)ENGINE = InnoDB CHARSET utf8 COLLATE utf8_general_ci;
+	`address_id` INT(10) NOT NULL AUTO_INCREMENT,
+    `address_zipcode` CHAR(5) NOT NULL, 
+    `address_city` VARCHAR(50) NOT NULL,
+    `address_number` VARCHAR(10),
+    `address_street_name` VARCHAR(100) NOT NULL,
+    CONSTRAINT PK_addresses PRIMARY KEY (`address_id`)
+)
+ENGINE = InnoDB
+CHARSET utf8
+COLLATE utf8_general_ci;
 
-CREATE TABLE Clients
+CREATE TABLE `Clients`
 (
-	client_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    client_name VARCHAR(50) NOT NULL,
-    client_firstname VARCHAR(50) NOT NULL,
-    client_enrollment DATETIME NOT NULL, 
-    client_deposit TINYINT(3)
-)ENGINE = InnoDB CHARSET utf8 COLLATE utf8_general_ci;
+	`client_id` INT(10) NOT NULL AUTO_INCREMENT,
+    `client_name` VARCHAR(50) NOT NULL,
+    `client_firstname` VARCHAR(50) NOT NULL,
+    `client_enrollment` DATETIME NOT NULL, 
+    `client_deposit` TINYINT(3),
+    CONSTRAINT PK_clients PRIMARY KEY (`client_id`)
+)
+ENGINE = InnoDB
+CHARSET utf8
+COLLATE utf8_general_ci;
 
-CREATE TABLE Shops
+CREATE TABLE `Shops`
 (
-	shop_name VARCHAR(50) NOT NULL,
-    address_id INT(10) NOT NULL,
-    CONSTRAINT fk_address_id FOREIGN KEY (address_id) REFERENCES Addresses(address_id),
-    CONSTRAINT pk_shop_id PRIMARY KEY (shop_name, address_id)
-)ENGINE = InnoDB CHARSET utf8 COLLATE utf8_general_ci;
-
-CREATE TABLE Visit
+	`shop_name` VARCHAR(50) NOT NULL,
+    `address_id` INT(10) NOT NULL,
+    CONSTRAINT FK_address_to_shop FOREIGN KEY (`address_id`) REFERENCES Adresses(`address_id`),
+    CONSTRAINT PK_shops PRIMARY KEY (`shop_name`, `address_id`)
+)
+ENGINE = InnoDB
+CHARSET utf8
+COLLATE utf8_general_ci;
+/*
+CREATE TABLE Visits
 (
 	client_id INT(10) NOT NULL,
-    shop_id VARCHAR(50) NOT NULL,
-	CONSTRAINT fk_client_id FOREIGN KEY (client_id) REFERENCES Clients(client_id),
-    CONSTRAINT fk_shop_id FOREIGN KEY (shop_id) REFERENCES Shops(pk_shop_id)
-);
+    shop_name VARCHAR(50) NOT NULL,
+    address_id INT(10) NOT NULL,
+    CONSTRAINT FK_client_to_visit FOREIGN KEY (client_id) REFERENCES Clients(client_id),
+    CONSTRAINT FK_shop_to_visit FOREIGN KEY (shop_name, address_id) REFERENCES Shops(shop_name, address_id),
+    CONSTRAINT PK_visit PRIMARY KEY (client_id, shop_name, address_id)
+)ENGINE = InnoDB CHARSET utf8 COLLATE utf8_general_ci;
+*/
